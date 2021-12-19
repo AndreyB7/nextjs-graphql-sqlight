319928229059eb48eb5d02c24e90bbb017c5c0f1
AndreyB7 <0129507@gmail.com>
2021-12-19 11:43:14 +0700

    deploy upd

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 cd /home/ubuntu/www
-npm run generate
-npm run build
 sudo chmod 777 prisma prisma/dev.db
 pm2 start npm --name "next_app" -- start
--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 cd /home/ubuntu/www
 npm install
+npm run generate
+npm run build
